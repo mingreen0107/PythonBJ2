05640299943ecc7517b3a8a4dcabdf6b72e3fa1f
MINJU <71gnos@naver.com>
2024-05-22 01:09:18 +0900

    p

--- B_1929.py
+++ B_1929.py
@@ -1,16 +1,11 @@
 # M 이상 N 이하의 소수 출력
 
-M, N = map(int, input().split())
-sosu = []
+m,n = map(int,input().split())
 
-for i in range(M, N+1):
-    if i == 1:
-        continue
+*l, = range(n+1)
 
-    for j in range(2, int(i/2) + 1):
-        if (i%j == 0):
-            break
-        else:
-            sosu.append(i)
+for i in l[2:]:
+    if l[i] >= m:
+        print(i)
 
-print(set(sosu))
+    l[i::i]=n // i*[0]
