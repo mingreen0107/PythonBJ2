e6765746f0dede6b1737add9051319c52ec64ff0
MINJU <71gnos@naver.com>
2024-06-03 11:20:04 +0900

    p

--- B_1978.py
+++ B_1978.py
@@ -7,9 +7,11 @@ numbers = map(int, input().split())
 def is_prime(n):
     if n <= 1:
         return False
+
     for j in range(2, n):
         if n % j == 0:
             return False
+
     return True
 
 prime_count = sum(is_prime(n) for n in numbers)
--- B_9012.py
+++ B_9012.py
@@ -13,18 +13,12 @@ for i in range(T):
         elif j == ')':
             if stack:
                 stack.pop()
-
-            # 스택에 괄호가 없을경우
             else:
                 print("NO")
                 break
 
-    # break문으로 끊기지 않고 수행됐을 경우
     else:
-        # break문으로 안 끊기고 스택이 비어있는 경우
         if not stack:
             print("YES")
-
-        # break에서 안 걸렸더라도 스택에 괄호가 들어있을 경우
         else:
             print("NO")
