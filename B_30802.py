4e58549b85a2523595122bc4474a748885ccc15f
MINJU <71gnos@naver.com>
2024-06-14 22:09:24 +0900

    p

--- B_30802.py
+++ B_30802.py
@@ -1,6 +1,17 @@
 # S, M, L, XL, XXL, XXXL
 
 N = int(input())
-S, M, L, XL, XXL, XXXL = map(int, input().split())
+size = list(map(int, input().split()))
 T, P = map(int, input().split())
 
+ans = 0
+for i in range(len(size)):
+    if size[i]%T == 0:
+        ans += int(size[i]/T)
+    else:
+        ans += (int(size[i]/T) + 1)
+
+print(ans)
+
+ans2 = [int(N/P), N%P]
+print(*ans2)
